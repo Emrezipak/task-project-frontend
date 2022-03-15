e4219d48a00898e4b4016e10049628495c18178c
EmreZipak <zipakemre@gmail.com>
2022-03-16 01:24:55 +0300

    update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,6 +10,12 @@ export const HomePage=()=> {
       Hi, I have created two roles, admin and user, admin users can add and delete tasks, I created a project where the user can only see the tasks added to it
       With spring security, I developed a project where the user can log in and after logging in, a jwt token belonging to the user is generated and only logged in and authorized users can take action.
     </p>
+
+  </div>
+  <div className='card-footer'>
+  <p>Admin girişi</p>
+  <span>admin1234@gmail.com</span><br/>
+  <span>Password22.</span>
   </div>
 </div>
     </div>
